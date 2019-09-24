2941ff80237b9a4087f3724fa4a18c353ac6eb8a
Julian Villegas <julian.villegasplus@gmail.com>
2019-09-24 14:43:42 +0000

    7. There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important. function that prints the last digit of a number. Prototype: int print_last_digit(int); Returns the value of the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - function that prints the last digit of a number.
+ * @x: The input number to check
+ * Return: int.
+ */
+
+int print_last_digit(int x)
+{
+	if (x < 0)
+		x = x * (-1);
+
+	x %= 10;
+	_putchar(x + '0');
+
+	return (x);
+}
