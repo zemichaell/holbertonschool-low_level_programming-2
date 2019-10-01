78c956d15be79da035d5b559dec7ddf7cbcc7f3a
Julian Villegas <julian.villegasplus@gmail.com>
2019-10-01 20:36:03 +0000

    Fix 7-puts_half.c file 2

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,18 +11,13 @@ void puts_half(char *str)
 {
 	int i = 0;
 	int n = 0;
-	char half;
 
 	while (str[i] != '\0')
 		i += 1;
-	if (i % 2 == 1)
-		n = (i - 1) / 2;
-	else
-		n = i / 2;
+	n = i / 2;
 	while (str[n] != '\0')
 	{
-		half = str[n];
-		_putchar(half);
+		_putchar(*(str + n));
 		n++;
 	}
 	_putchar('\n');
