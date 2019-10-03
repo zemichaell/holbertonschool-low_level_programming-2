18ea38d9094b369467e8cfc10ed31486156dd706
Julian Villegas <julian.villegasplus@gmail.com>
2019-10-03 17:57:02 +0000

    Fix 2-strncpy.c file 5

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i < n && *(dest + i) != '\0')
+	while (i < n && *(src + i) != '\0')
 	{
 		*(dest + i) = *(src + i);
-		if (*(src + i) == '\0')
-			*(dest + i) = '\0';
 		i++;
 	}
 	while (i != n)
