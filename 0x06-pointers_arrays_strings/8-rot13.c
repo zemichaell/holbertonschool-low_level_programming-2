c7b1a34b8f74899a0f9a0c7e57c9d00fc4f8c5c6
Julian Villegas <julian.villegasplus@gmail.com>
2019-10-04 05:02:07 +0000

    Header and function that encodes a string using rot13.

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * rot13 - function that encodes a string using rot13.
+ * @s: Pointer to Char
+ * Return: char.
+ */
+
+char *rot13(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if ((*(s + i) >= 'a' && *(s + i) <= 'm')
+			|| (*(s + i) >= 'A' && *(s + i) <= 'M'))
+		{
+			*(s + i) = *(s + i)  + 13;
+		}
+		else
+			while ((*(s + i) >= 'n' && *(s + i) <= 'z')
+				|| (*(s + i) >= 'N' && *(s + i) <= 'Z'))
+			{
+				*(s + i) = *(s + i) - 13;
+			}
+		i++;
+	}
+	return (s);
+}
