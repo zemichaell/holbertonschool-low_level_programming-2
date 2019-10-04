4a6db9dc9141cdbbc3cba999e7f4b517fc6aaa15
Julian Villegas <julian.villegasplus@gmail.com>
2019-10-04 05:58:22 +0000

    Fix 8-rot13.c file 4

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -9,19 +9,22 @@
 char *rot13(char *s)
 {
 	int i = 0;
+	int j = 0;
+	char *l = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char *r13 = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
 
-	while (s[i] != '\0')
+	while (*(s + i) != '\0')
 	{
-		if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+		while (*(l + j) != '\0')
 		{
-			s[i] = (s[i] + 13);
-		}
-		else
-			while ((s[i] >= 'n' && s[i] <= 'z') ||
-			       (s[i] >= 'N' && s[i] <= 'Z'))
+			if (*(s + i) == *(l + j))
 			{
-				s[i] = (s[i] - 13);
+				*(s + i)  = *(r13 + j);
+				break;
 			}
+			j++;
+		}
+		j = 0;
 		i++;
 	}
 	return (s);
