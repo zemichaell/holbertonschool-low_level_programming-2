f07519ce7db17705396f29987d0bf369bf26a116
Julian Villegas <julian.villegasplus@gmail.com>
2019-10-17 03:33:23 +0000

    Fix 3-alloc_grid.c file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,14 +10,25 @@
 int **alloc_grid(int width, int height)
 {
 	char **arr;
+	int i, j, x;
 
+	i = j = x = 0;
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	arr = malloc((width * height) * sizeof(int));
+	/* using typecasting (int *) to dereference values */
+	arr = malloc(height * sizeof(int *));
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; arr[i] < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		for (j = 0; a)
+		arr[i] = malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+			return (NULL);
 	}
+	for (j = 0; j < height; j++)
+	{
+		for (x = 0; x < width; x++)
+			arr[j][x] = 0;
+	}
+	return (arr);
 }
