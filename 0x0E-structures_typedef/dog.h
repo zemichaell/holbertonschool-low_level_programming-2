d314aee3aa86360381e05aece9517558027a3e0a
Julian Villegas <julian.villegasplus@gmail.com>
2019-10-22 04:39:25 +0000

    new type struct dog with custom fields

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struture of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+int _putchar(char c);
+#endif /* #ifndef DOG_H */
