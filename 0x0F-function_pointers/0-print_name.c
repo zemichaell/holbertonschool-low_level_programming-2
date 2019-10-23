0978983540f18c2d50ab423909429f3f4fb355a7
Julian Villegas <julian.villegasplus@gmail.com>
2019-10-23 18:39:27 +0000

    Header and function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: name of the person
+ * @f: input pointer function and execute it
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,5 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+#endif /* #ifndef HOLBERTON_H */
