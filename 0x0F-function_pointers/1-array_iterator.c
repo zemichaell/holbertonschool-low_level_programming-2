7ff0e0236a829886620a6f7f275a947a631fc13e
Julian Villegas <julian.villegasplus@gmail.com>
2019-10-23 20:09:52 +0000

    [FIX]: in conditional

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
-	if (array != 0 || size > 0 || action != 0)
+	if (array != 0 && size >= 1 && action != 0)
 		while (i < size)
 		{
 			action(array[i]);
