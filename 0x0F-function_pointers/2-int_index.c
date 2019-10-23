50e5ceb672edd718b2104be82c470a627e4ed349
Julian Villegas <julian.villegasplus@gmail.com>
2019-10-23 21:02:00 +0000

    [FIX]: Adding conditionals for NULL

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,11 +13,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
-	for (i = 0; i < size; i++)
-		if (cmp(array[i]))
-			return (i);
+	if (array != 0 && cmp != 0)
+	{
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
 	/* If element not found */
 	return (-1);
 }
