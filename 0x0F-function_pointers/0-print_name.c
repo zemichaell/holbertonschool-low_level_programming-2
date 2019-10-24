9131eb16ac6a3874e50d3f4b88f50c3485b837a5
Julian Villegas <julian.villegasplus@gmail.com>
2019-10-24 06:12:25 +0000

    [FIX]: change logic

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
 /**
@@ -9,6 +10,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != 0)
+	if (name != 0 && f != 0)
 		f(name);
 }
