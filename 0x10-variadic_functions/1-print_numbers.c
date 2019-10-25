fd4096b11714db391241157363b7b0b457002541
Julian Villegas <julian.villegasplus@gmail.com>
2019-10-25 18:24:33 +0000

    [ADD]1-print: Header and function that prints numbers, followed by a new line

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that prints numbers
+ * @separator: string to be printed between numbers
+ * @n: number of parameters
+ * Return: Sum of all parameters
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	if (separator != 0 || n != 0)
+	{
+		va_list arguments;
+		unsigned int i;
+		int aux = 0;
+
+		va_start(arguments, n);
+		for (i = 0; i < n; i++)
+		{
+			aux = va_arg(arguments, int);
+			if (i == n - 1)
+				printf("%d", aux);
+			else
+				printf("%d%c", aux, *separator);
+		}
+		printf("\n");
+		va_end(arguments);
+	}
+}
