1966c46458757d0b20137ace8aa80e964a51b1a1
Julian Villegas <julian.villegasplus@gmail.com>
2019-10-26 23:57:09 +0000

    [ADD]3-print_all: Header and function that prints anything

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,4 +4,19 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct type - Struct data type
+ *
+ * @op: data type argument
+ * @f: The function associated
+ */
+
+typedef struct type
+{
+	char *op;
+	void (*f)(va_list, char *);
+} type_t;
+
 #endif
