26d818eed55c4f091e0b22a0f2c6340baa86fffd
Julian Villegas <julian.villegasplus@gmail.com>
2019-10-26 20:06:28 +0000

    [FIX]1-print_numers: Change conditional of separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,19 +14,18 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	int aux = 0;
 
-	if (separator != 0 && n != 0)
-	{
 
-		va_start(arguments, n);
-		for (i = 0; i < n; i++)
-		{
-			aux = va_arg(arguments, int);
-			if (i == n - 1)
-				printf("%d", aux);
-			else
-				printf("%d%s", aux, separator);
-		}
-		printf("\n");
+	va_start(arguments, n);
+	for (i = 0; i < n; i++)
+	{
+		aux = va_arg(arguments, int);
+		if (separator == 0)
+			printf("%d", aux);
+		if (i == n - 1)
+			printf("%d", aux);
+		else
+			printf("%d%s", aux, separator);
 	}
+	printf("\n");
 	va_end(arguments);
 }
