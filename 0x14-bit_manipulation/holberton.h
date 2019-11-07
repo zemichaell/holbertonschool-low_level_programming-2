5631dd27573db44ae941cadd8099f9139af3ce8b
Julian Villegas <julian.villegasplus@gmail.com>
2019-11-07 21:03:15 +0000

    [ADD]0-binary_to_uint

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,6 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+unsigned int binary_to_uint(const char *b);
+/*int _strlen(char *s);
+  int _pow_recursion(int x, int y);*/
+#endif /* #ifndef _HOLBERTON_H */
