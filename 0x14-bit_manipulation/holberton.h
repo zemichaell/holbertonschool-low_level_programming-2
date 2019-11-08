3972182c71e7e5d51cf97760eeece1a4d025903a
Julian Villegas <julian.villegasplus@gmail.com>
2019-11-08 04:02:42 +0000

    [ADD]1-print_binary: Header and function that prints the binary representation of a number

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,4 +1,6 @@
 #ifndef _HOLBERTON_H
 #define _HOLBERTON_H
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 #endif /* #ifndef _HOLBERTON_H */
