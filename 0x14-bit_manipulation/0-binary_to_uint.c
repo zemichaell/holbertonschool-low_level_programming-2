89e47782f46fcaea7eee500c094cc40009c212fa
Julian Villegas <julian.villegasplus@gmail.com>
2019-11-08 02:24:12 +0000

    [UPD]0-binary_to_uint: Header and  function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -38,7 +38,7 @@ int _strlen(const char *s)
 }
 
 /**
- * binary_to_unit - function that converts a binary number to an unsigned int
+ * binary_to_uint - function that converts a binary number to an unsigned int
  * @b: pointing to a string of 0 and 1 chars
  * Return: the converted number, or 0 if
  * there is one or more chars in the string b that is not 0 or 1
@@ -50,11 +50,15 @@ unsigned int binary_to_uint(const char *b)
 	int len, exp = 0;
 	unsigned int res = 0;
 
+	if (b == NULL)
+		return (0);
 	len = _strlen(b);
 	while (len-- && len >= 0)
 	{
 		if (b[len] == '1')
 			res += _pow_recursion(2, exp);
+		else if (b[len] != '0')
+			return (0);
 		exp++;
 	}
 	return (res);
