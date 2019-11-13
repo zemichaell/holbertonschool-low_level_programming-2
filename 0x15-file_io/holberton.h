684af09ce32f20317d0d1429251976de4b3c5c45
Julian Villegas <julian.villegasplus@gmail.com>
2019-11-13 03:58:29 +0000

    [ADD]1-create_file: Header and function that creates a file

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,4 +1,5 @@
 #ifndef _HOLBERTON_H
 #define _HOLBERTON_H
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif /* #ifndef _HOLBERTON_H */
