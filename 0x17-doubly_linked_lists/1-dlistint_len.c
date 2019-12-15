850463b8a7e6998b929536493374ccef923e2b21
Julian Villegas <julian.villegasplus@gmail.com>
2019-12-15 17:46:55 +0000

    [FIX]1-dlistint_len: betty style

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,9 +3,9 @@
 #include "lists.h"
 
 /**
- * list_len - function that returns the number of elements in a linked
+ * dlistint_len - function that returns the number of elements in a linked
  * list_t list
- * @h: header pointer
+ * @h: h pointer
  * Return: number of elements
  */
 size_t dlistint_len(const dlistint_t *h)
