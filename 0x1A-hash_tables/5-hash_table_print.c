292473fbdc43ca9dcdfb8c67f7fe14f6ccaac1aa
Julian Villegas <julian.villegasplus@gmail.com>
2020-01-20 06:06:20 +0000

    [FIX]5-hash_table_print: Changing all structure of function to optimize code

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,71 +7,25 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int i, count, count2, size;
-	char *key, *value;
-	hash_node_t *tmp;
+	unsigned int i;
+	hash_node_t *tmp = NULL;
+	char *d = "";
 
 	if (ht == NULL)
 		return;
-	size = ht->size;
-	count = count2 = 0;
-	count = count_nodes(ht);
 	printf("{");
-	for (i = 0; i < size; i++)
-	{
-		if (ht->array[i] != NULL)
-		{
-			tmp = ht->array[i];
-			if (tmp->next != NULL)
-			{
-				count++;
-				while (tmp != NULL)
-				{
-					count2++, key = tmp->key, value = tmp->value;
-					if (count2 != count)
-						printf("'%s': '%s', ", key, value);
-					else
-						printf("'%s': '%s'", key, value);
-					tmp = tmp->next;
-				}
-			}
-			else
-			{
-				key = tmp->key, value = tmp->value;
-				if (count2 != count - 1)
-					printf("'%s': '%s', ", key, value);
-				else
-					printf("'%s': '%s'", key, value);
-				count2++;
-			}
-		}
-	}
-	printf("}\n");
-}
-
-/**
- * count_nodes - counts the number of nodes
- * @ht: hash table
- * Return: count
- */
-unsigned long int count_nodes(const hash_table_t *ht)
-{
-	unsigned long int i, count;
-	hash_node_t *temp;
-
-	count = 0;
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
 		{
-			temp = ht->array[i];
-			if (temp->next != NULL)
+			tmp = ht->array[i];
+			while (tmp != NULL)
 			{
-				while (temp != NULL)
-					temp = temp->next;
+				printf("%s\'%s\': \'%s\'", d, tmp->key, tmp->value);
+				d = ", ";
+				tmp = tmp->next;
 			}
-			count++;
 		}
 	}
-	return (count);
+	printf("}\n");
 }
